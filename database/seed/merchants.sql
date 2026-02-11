1a9cdf63b9ab2393fff3e1cf0d38eb3689af260a
Arunkumar Senthilraja <arunkpillai73@gmail.com>
2026-02-11 12:26:31 +0530

    Added More Data

--- database/seed/merchants.sql
+++ database/seed/merchants.sql
@@ -1,7 +1,35 @@
 INSERT INTO merchants (merchant_name, category_id) VALUES
+-- Food
 ('Swiggy', 1),
 ('Zomato', 1),
+('Dominos', 1),
+('KFC', 1),
+('Subway', 1),
+('BigBasket', 1),
+
+-- Transport
 ('Uber', 2),
+('Ola', 2),
+('Rapido', 2),
+('IRCTC', 2),
+('MakeMyTrip', 2),
+
+-- Shopping
 ('Amazon', 3),
+('Flipkart', 3),
+('Myntra', 3),
+('Ajio', 3),
+('Paytm Mall', 3),
+
+-- Entertainment
 ('Netflix', 4),
-('Electricity Board', 5);
+('Hotstar', 4),
+('BookMyShow', 4),
+('Spotify', 4),
+
+-- Utilities
+('Electricity Board', 5),
+('Airtel', 5),
+('Jio', 5),
+('BSNL', 5),
+('Tata Power', 5);
